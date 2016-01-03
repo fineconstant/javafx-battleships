b3606e1e3f34ae9bfdaeec9ef4f219af0d76da85
Kamil Duda <kamilduda01@gmail.com>
2016-01-03 19:45:16 +0100

    Add Cell.java properties

--- src/com/kduda/battleships/models/Cell.java
+++ src/com/kduda/battleships/models/Cell.java
@@ -1,6 +1,22 @@
 package com.kduda.battleships.models;
 
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
+
 public class Cell extends Rectangle {
+    public int x, y;
+    public Unit unit = null;
+    public boolean wasShot = false;
+
+    private Board board;
+
+    public Cell(int x, int y, Board board) {
+        super(30, 30);
+        this.x = x;
+        this.y = y;
+        this.board = board;
+        setFill(Color.LIGHTGRAY);
+        setStroke(Color.BLACK);
+    }
 }
