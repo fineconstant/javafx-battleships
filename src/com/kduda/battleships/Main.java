243a25ceedd1674069f0c174672451135f58b90c
Kamil Duda <kamilduda01@gmail.com>
2016-01-03 13:05:04 +0100

    Change to default look and feel

--- src/com/kduda/battleships/Main.java
+++ src/com/kduda/battleships/Main.java
@@ -15,7 +15,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("views/battleshipsScene.fxml"));
-        setUserAgentStylesheet(STYLESHEET_CASPIAN);
+//        setUserAgentStylesheet(STYLESHEET_CASPIAN);
         primaryStage.setTitle("Battleships");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
