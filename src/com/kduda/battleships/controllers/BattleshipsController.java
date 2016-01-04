7b1fcb9bd09709b74e509361d92764c84c62f52b
Kamil Duda <kamilduda01@gmail.com>
2016-01-04 16:38:48 +0100

    Add boards initialize method

--- src/com/kduda/battleships/controllers/BattleshipsController.java
+++ src/com/kduda/battleships/controllers/BattleshipsController.java
@@ -1,18 +1,69 @@
 package com.kduda.battleships.controllers;
 
+import com.kduda.battleships.models.Board;
+import com.kduda.battleships.models.Cell;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class BattleshipsController implements Initializable {
-    public GridPane myBoard;
-    public GridPane opponentBoard;
+    public VBox enemyBoardArea;
+    public VBox playerBoardArea;
+    Board enemyBoard;
+    Board playerBoard;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        initializeBoards();
+    }
+
+    private void initializeBoards() {
+        enemyBoard = new Board(true, e -> {
+            //TODO: click handler
+            //TODO: hover handler
+        });
+        enemyBoardArea.getChildren().add(enemyBoard);
+
+        playerBoard = new Board(false, e -> {
+            //TODO: click handler
+            //TODO: hover handler
+        });
+        playerBoardArea.getChildren().addAll(playerBoard);
+
+
+        //HANDLERS
+
+//        enemyBoard = new Board(true, event -> {
+//            if (!running)
+//                return;
+//
+//            Cell cell = (Cell) event.getSource();
+//            if (cell.wasShot)
+//                return;
+//
+//            enemyTurn = !cell.shootCell();
+//
+//            if (enemyBoard.ships == 0) {
+//                System.out.println("YOU WIN");
+//                System.exit(0);
+//            }
+//
+//            if (enemyTurn)
+//                enemyMove();
+//        });
+
+//        playerBoard = new Board(false, event -> {
+//            if (running)
+//                return;
+//
+//            Cell cell = (Cell) event.getSource();
+//            if (playerBoard.placeShip(new Ship(shipsToPlace, event.getButton() == MouseButton.PRIMARY), cell.x, cell.y)) {
+//                if (--shipsToPlace == 0) {
+//                    startGame();
+//                }
+//            }
+//        });
     }
 }
