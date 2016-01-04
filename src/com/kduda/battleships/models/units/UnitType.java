ed34cb24b856ef438607079604519eaadb22374c
Kamil Duda <kamilduda01@gmail.com>
2016-01-04 18:30:08 +0100

    Reduct unit types

--- src/com/kduda/battleships/models/units/UnitType.java
+++ src/com/kduda/battleships/models/units/UnitType.java
@@ -1,12 +1,7 @@
 package com.kduda.battleships.models.units;
 
 public enum UnitType {
-    Ship1,
-    Ship2,
-    Ship3,
-    Ship4,
-    Tank2,
-    Tank3,
-    Tank4,
+    Ship,
+    Tank,
     Plane
 }
