9fe21da279ba55442e219ee67f4c5a38dce7d84b
Kamil Duda <kamilduda01@gmail.com>
2016-01-04 17:36:21 +0100

    Add Position class for int coordinates

---
+++ src/com/kduda/battleships/models/Position.java
@@ -0,0 +1,19 @@
+package com.kduda.battleships.models;
+
+public class Position {
+    private final int X;
+    private final int Y;
+
+    public Position(int x, int y) {
+        this.X = x;
+        this.Y = y;
+    }
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+}
