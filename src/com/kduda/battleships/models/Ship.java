758895cf5ca995a50da7c0e79bb6ecc6e5fcb535
Kamil Duda <kamilduda01@gmail.com>
2016-01-04 15:05:49 +0100

    Add Ship implementation Ship.java

---
+++ src/com/kduda/battleships/models/Ship.java
@@ -0,0 +1,23 @@
+package com.kduda.battleships.models;
+
+public class Ship implements Unit {
+    public int type;
+    public boolean vertical = true;
+    private int health;
+
+    public Ship(int type, boolean vertical) {
+        this.type = type;
+        this.vertical = vertical;
+        this.health = type;
+    }
+
+    @Override
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
+    @Override
+    public void hit() {
+
+    }
+}
