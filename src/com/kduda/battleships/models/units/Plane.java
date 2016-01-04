f8815f1747717beb7400c40e6e530493a7207c92
Kamil Duda <kamilduda01@gmail.com>
2016-01-04 18:01:30 +0100

    Add Plane class

---
+++ src/com/kduda/battleships/models/units/Plane.java
@@ -0,0 +1,13 @@
+package com.kduda.battleships.models.units;
+
+public class Plane implements Unit {
+    @Override
+    public boolean isAlive() {
+        return false;
+    }
+
+    @Override
+    public void hit() {
+
+    }
+}
