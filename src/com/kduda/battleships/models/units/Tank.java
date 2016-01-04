44664aa20ea12987781567471752632f3c1f204d
Kamil Duda <kamilduda01@gmail.com>
2016-01-04 18:01:41 +0100

    Add Tank class

---
+++ src/com/kduda/battleships/models/units/Tank.java
@@ -0,0 +1,13 @@
+package com.kduda.battleships.models.units;
+
+public class Tank implements Unit {
+    @Override
+    public boolean isAlive() {
+        return false;
+    }
+
+    @Override
+    public void hit() {
+
+    }
+}
