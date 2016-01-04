79940b89d19ed476126d3b24fc31862353e9a0b3
Kamil Duda <kamilduda01@gmail.com>
2016-01-04 18:30:19 +0100

    Factory method as singleton

--- src/com/kduda/battleships/models/units/UnitFactory.java
+++ src/com/kduda/battleships/models/units/UnitFactory.java
@@ -1,4 +1,10 @@
 package com.kduda.battleships.models.units;
 
-public class UnitFactory {
+public enum UnitFactory {
+    INSTANCE;
+
+    Unit createUnit(int size, UnitType type) {
+
+        return null;
+    }
 }
