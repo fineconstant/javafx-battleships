b16fbc776a7630198f24e58633f660fe39d6af74
Kamil Duda <kamilduda01@gmail.com>
2016-01-05 14:47:41 +0100

    Add ctor with units list initialization

--- src/com/kduda/battleships/models/units/UnitFactory.java
+++ src/com/kduda/battleships/models/units/UnitFactory.java
@@ -1,21 +1,57 @@
 package com.kduda.battleships.models.units;
 
-import javafx.geometry.Orientation;
+import java.util.ArrayList;
 
 public enum UnitFactory {
     INSTANCE;
+    ArrayList<Unit> units;
 
-    public Unit createGroundLevelUnit(UnitType type, int size, Orientation orientation) {
+    UnitFactory() {
+        units = new ArrayList<>();
+        units.add(createGroundLevelUnit(UnitType.Ship, 4));
+
+        units.add(createGroundLevelUnit(UnitType.Ship, 3));
+        units.add(createGroundLevelUnit(UnitType.Ship, 3));
+
+        units.add(createGroundLevelUnit(UnitType.Ship, 2));
+        units.add(createGroundLevelUnit(UnitType.Ship, 2));
+        units.add(createGroundLevelUnit(UnitType.Ship, 2));
+
+        units.add(createGroundLevelUnit(UnitType.Ship, 1));
+        units.add(createGroundLevelUnit(UnitType.Ship, 1));
+        units.add(createGroundLevelUnit(UnitType.Ship, 1));
+
+        units.add(createGroundLevelUnit(UnitType.Tank, 4));
+
+        units.add(createGroundLevelUnit(UnitType.Tank, 3));
+        units.add(createGroundLevelUnit(UnitType.Tank, 3));
+
+        units.add(createGroundLevelUnit(UnitType.Tank, 2));
+        units.add(createGroundLevelUnit(UnitType.Tank, 2));
+        units.add(createGroundLevelUnit(UnitType.Tank, 2));
+
+
+        units.add(createPlane());
+        units.add(createPlane());
+        units.add(createPlane());
+    }
+
+    public Unit createNextUnit() {
+
+        return null;
+    }
+
+    Unit createGroundLevelUnit(UnitType type, int size) {
         switch (type) {
             case Ship:
-                return new Ship(size, orientation);
+                return new Ship(size);
             case Tank:
-                return new Tank(size, orientation);
+                return new Tank(size);
         }
         return null;
     }
 
-    public Unit createPlane(int size, Direction direction) {
-        return new Plane(size, direction);
+    Unit createPlane() {
+        return new Plane();
     }
 }
