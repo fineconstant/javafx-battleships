41aa0b3fe9aad650ec1bc095c4197904019c6201
Kamil Duda <kamilduda01@gmail.com>
2016-01-06 14:38:01 +0100

    Add plane rotate method

--- src/com/kduda/battleships/models/units/Plane.java
+++ src/com/kduda/battleships/models/units/Plane.java
@@ -1,11 +1,13 @@
 package com.kduda.battleships.models.units;
 
+import static com.kduda.battleships.models.units.Direction.*;
+
 public class Plane extends Unit {
     private Direction direction;
 
     public Plane() {
         super(5);
-        this.direction = Direction.North;
+        this.direction = North;
     }
 
     public Direction getDirection() {
@@ -18,6 +20,19 @@ public class Plane extends Unit {
 
     @Override
     public void rotateUnit() {
-
+        switch (this.direction) {
+            case North:
+                this.direction = East;
+                break;
+            case East:
+                this.direction = South;
+                break;
+            case South:
+                this.direction = West;
+                break;
+            case West:
+                this.direction = North;
+                break;
+        }
     }
 }
